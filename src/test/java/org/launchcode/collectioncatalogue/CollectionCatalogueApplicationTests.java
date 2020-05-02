196d19c4e4c7519bf865f7794c9fb55204901cf4
Alex <avmetzger@gmail.com>
2020-05-02 18:26:58 -0500

    initialized project with index page

---
+++ src/test/java/org/launchcode/collectioncatalogue/CollectionCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.collectioncatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
